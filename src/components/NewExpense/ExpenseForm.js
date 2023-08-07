2f1a2859735c63e3fc237c2973e54a9d89603ed5
hyeonjin <gdalove@naver.com>
2023-08-07 21:02:06 +0900

    추가

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -14,12 +14,18 @@ const ExpenseForm = (props) => {
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+    //상태를 업데이트 할 때, 이전 상태에 의존할 경우 > 상태 업데이트 함수의 대체 양식을 사용해야함
     // setUserInput({
     //   ...userInput,
     //   enteredTitle: event.target.value,
     // });
+    // 업데이트 함수를 호출하는 그상태에 대한 이전상태 스냅샷을 받음
+    //리액트는 상태 업데이트를 예약함 즉시 처리 하지 않음
+    // 내부 함수에서 제공하는 상태 스냅샷이 항상 최신 상태 스냅샷이 되도록 보장해줌
     // setUserInput((prevState) => {
-    //   return { ...prevState, enteredTitle: event.target.value };
+    //전개 연산자에서 키 값 쌍을 복사
+    //   return { ...prevState, enteredTitle: event.target.value }; //새상태 리턴
+
     // });
   };
 
@@ -53,6 +59,10 @@ const ExpenseForm = (props) => {
     setEnteredAmount('');
     setEnteredDate('');
   };
+
+  //양방향 바인딩
+
+  // 인풋내용을 수신하는것 뿐 아니라 인풋 요소에 새 값을 넣읋수 있음 >> value 속성을 사용
 
   return (
     <form onSubmit={submitHandler}>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,8 +4,14 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  /**
+   *
+   * 1. 지출데이터를 newExpense.js로 전달
+   */
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
+      //새 객체를 만들어줌 >> submitHandler에서 생성한 객체가 들어옴
       // expenseForm의 값을 저장
       ...enteredExpenseData,
       id: Math.random().toString(),
@@ -17,6 +23,8 @@ const NewExpense = (props) => {
   return (
     <div className='new-expense'>
       {/* props로 함수를 전달  */}
+      {/* saveExpenseDataHandler 함수는 포인터로 지정 전달  */}
+      {/* 함수를 가르키는 포인터가 expenseForm에 전달  */}
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
